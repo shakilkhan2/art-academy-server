cdc0a05d8772492ee3181e2894ff7878e2587eb7
shakilkhan2 <dev.shakilkhan@gmail.com>
2023-06-09 16:21:02 +0600

    nodemon fixed

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rawumbi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,38 +25,38 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-const allData = client.db('artDB').collection('allInfo')
-const cartCollection = client.db('artDB').collection('carts')
+    const allData = client.db("artDB").collection("allInfo");
+    const cartCollection = client.db("artDB").collection("carts");
 
-// home collection
-app.get('/allInfo', async(req, res) => {
-    const result = await allData.find().toArray();
-    res.send(result);
-})
+    // home collection
+    app.get("/allInfo", async (req, res) => {
+      const result = await allData.find().toArray();
+      res.send(result);
+    });
 
-// cart collection
-app.get('/carts', async(req, res) => {
-    const email = req.query.email;
-    if(!email){
+    // cart collection
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
         res.send([]);
-    }
-   const query = {email: email};
-   const result = await cartCollection.find(query).toArray();
-   res.send(result)
-})
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
 
-app.post('/carts', async(req, res) => {
-    const course = req.body;
-    console.log(course);
-    const result = await cartCollection.insertOne(course);
-    res.send(result)
-})
-
-
+    app.post("/carts", async (req, res) => {
+      const course = req.body;
+      console.log(course);
+      const result = await cartCollection.insertOne(course);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -66,12 +64,10 @@ app.post('/carts', async(req, res) => {
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
-    res.send("The Art Academy Server is running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`Art Academy is running on port: ${port}`);
-  });
+  res.send("The Art Academy Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Art Academy is running on port: ${port}`);
+});
